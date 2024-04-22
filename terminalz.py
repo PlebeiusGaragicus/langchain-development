5d55d7468e7815e107bf174913fbc30c78f0e18a
PlebbyG <plebeiusgaragicus@gmail.com>
2024-04-22 10:40:38 -0700

    proper routing!

--- terminalz.py
+++ terminalz.py
@@ -36,50 +36,165 @@ def reset_color():
 
 
 import os
-from typing import Annotated
+from typing import Annotated, Literal
 
 from typing_extensions import TypedDict
 
-from langgraph.graph import StateGraph
-from langgraph.graph.message import add_messages
 from langchain_core.messages import HumanMessage, AIMessage
-
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain.prompts import PromptTemplate
 
 from langchain_community.chat_models import ChatOllama
 
+from langgraph.graph import END, StateGraph
+from langgraph.graph.message import add_messages
 
+# from langchain_openai import ChatOpenAI
+
+
+
+
+# workflow = StateGraph(GraphState)
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
-# def chatbot(state: State):
-#     return {"messages": [llm.invoke(state["messages"])]}
 
-def chatbot(state: State, config):
-    model="llama3"
+
+
+
+
+
+
+
+
+
+
+def route_question(state: State, config):
+    """
+    Route question to web search or RAG.
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Next node to call
+    """
+
+    cprint("--- NODE: route_question() ---", Colors.MAGENTA)
+    convo_history = state["messages"]
+    # question = state["question"]
+    # print(convo_history)
+
+
+    model = "llama3"
+    llm = ChatOllama(model=model, format="json", temperature=0)
+
+    prompt = PromptTemplate(
+        template="""You are an expert at routing a user conversations to the next appropriate workflow: \n
+
+        Use 'vectorstore' for questions related to firefighting operations, policies, terminology or knowledge. \n
+        Use 'friendly_chatbot' for all other user questions or replies. \n
+
+        You do not need to be stringent with the keywords in the question related to these topics. \n
+        Give a binary choice 'vectorstore' or 'friendly_chatbot' based on the question. \n
+        Return JSON with a single key 'datasource' and no premable or explaination. \n
+        Conversation history to route: {convo_history}""",
+        input_variables=["convo_history"],
+    )
+
+    question_router = prompt | llm | JsonOutputParser()
+
+    source = question_router.invoke({"convo_history": convo_history}, config=config)
+    # print(source)
+    # print(source['datasource'])
+
+    if source['datasource'] == 'vectorstore':
+        print("---ROUTE QUESTION TO vectorstore---")
+        return "vectorstore"
+    elif source['datasource'] == 'friendly_chatbot':
+        print("---ROUTE QUESTION TO friendly chatbot---")
+        return "friendly_chatbot"
+    else:
+        print("---ROUTING ERROR---")
+        return "bad_route"
+
+
+
+
+
+def friendly_chatbot(state: State, config):
+    cprint("--- NODE: friendly_chatbot() ---", Colors.MAGENTA)
+
+    model="llama3" # TODO - pull the model, temperature, etc from the config!
     llm = ChatOllama(model=model)
 
-    print(config)
+    user_input = state["messages"][-1].content
 
-    # output = ""
-    # change_color(Colors.GREEN)
-    # async for chunk in llm.astream(state["messages"], config=config):
-    #     # print(chunk.content, end="", flush=True)
-    #     output += chunk.content
-    # reset_color()
-    # return {"messages": [output]}
+    prompt = PromptTemplate(
+            template="""You are the human's friend.\n
+            Keep dialog short, don't use proper grammar or punctuation.\n
+            Your friend said: {user_input}.\n""",
+            input_variables=["user_input"],
+        )
 
-    return {"messages": [llm.invoke(state["messages"], config=config)]}
-    # return  for chunk in llm.astream(state["messages"], config=config):
+    chain = prompt | llm
 
+    return {"messages": [chain.invoke({"user_input": user_input}, config=config)]}
+        # state["messages"], config=config)]}
+
+
+def vectorstore(state: State, config):
+    cprint("--- NODE: vectorstore() ---", Colors.MAGENTA)
+
+    return {"messages": [AIMessage(content="THE DATABASE IS NOT YET IMPLEMENTED!")]}
+
+
+def bad_route(state: State, config):
+    cprint("--- NODE: bad_route() ---", Colors.MAGENTA)
+
+    return {"messages": [AIMessage(content="I'm sorry, I don't understand that question.")]}
 
 
 graph_builder = StateGraph(State)
-graph_builder.add_node("chatbot", chatbot)
-graph_builder.set_entry_point("chatbot")
-graph_builder.set_finish_point("chatbot")
+
+graph_builder.add_node("vectorstore", vectorstore)
+graph_builder.add_edge("vectorstore", END)
+graph_builder.add_node("friendly_chatbot", friendly_chatbot)
+graph_builder.add_edge("friendly_chatbot", END)
+graph_builder.add_node("bad_route", bad_route)
+graph_builder.add_edge("bad_route", END)
+
+graph_builder.set_conditional_entry_point(
+                route_question,
+                {
+                    "vectorstore": "vectorstore",
+                    "friendly_chatbot": "friendly_chatbot",
+                    "bad_route": "bad_route"
+                }
+            )
+
+# graph_builder.set_entry_point("friendly_chatbot")
+# graph_builder.set_finish_point("friendly_chatbot")
 
 graph = graph_builder.compile()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -130,6 +245,8 @@ async def main():
 
 
             if event['event'] == 'on_chat_model_stream':
+                if event['data']['chunk'].content.strip() == "":
+                    continue
                 chunk = event['data']['chunk'].content
                 cput(chunk, Colors.GREEN)
 
@@ -137,8 +254,8 @@ async def main():
                 cprint(event['event'], Colors.YELLOW)
                 try:
                     # TODO - this tries to get the penultimate graph output - this will break as my graph changes!
-                    last_chain_ending = event['data']["output"]['chatbot']['messages'][0].content
-                except KeyError:
+                    last_chain_ending = event['data']["output"]['friendly_chatbot']['messages'][0].content
+                except (KeyError, TypeError):
                     pass
             else:
                 cprint(event['event'], Colors.YELLOW)
@@ -162,31 +279,3 @@ if __name__ == "__main__":
 CONFIG = """
 {'tags': [], 'metadata': {}, 'callbacks': <langchain_core.callbacks.manager.CallbackManager object at 0x1040eb400>, 'recursion_limit': 25, 'something': 'yes, yes, it is!', 'configurable': {'__pregel_send': <built-in method extend of collections.deque object at 0x1040e59c0>, '__pregel_read': functools.partial(<function _local_read at 0x10206b400>, {'v': 1, 'ts': '2024-04-22T06:36:19.820879+00:00', 'channel_values': {}, 'channel_versions': defaultdict(<class 'int'>, {'__start__': 1, 'messages': 2, 'start:chatbot': 2}), 'versions_seen': defaultdict(<function _seen_dict at 0x101b6af80>, {'__start__': defaultdict(<class 'int'>, {'__start__': 1}), 'chatbot': defaultdict(<class 'int'>, {'start:chatbot': 2})})}, {'messages': <langgraph.channels.binop.BinaryOperatorAggregate object at 0x104076740>, '__start__': <langgraph.channels.ephemeral_value.EphemeralValue object at 0x104077070>, 'chatbot': <langgraph.channels.ephemeral_value.EphemeralValue object at 0x104076830>, 'start:chatbot': <langgraph.channels.ephemeral_value.EphemeralValue object at 0x104077c40>}, deque([]))}}
 """
-
-
-
-
-# ### Router
-
-# from langchain.prompts import PromptTemplate
-# from langchain_community.chat_models import ChatOllama
-# from langchain_core.output_parsers import JsonOutputParser
-
-# # LLM
-# llm = ChatOllama(model=local_llm, format="json", temperature=0)
-
-# prompt = PromptTemplate(
-#     template="""You are an expert at routing a user question to a vectorstore or web search. \n
-#     Use the vectorstore for questions on LLM  agents, prompt engineering, and adversarial attacks. \n
-#     You do not need to be stringent with the keywords in the question related to these topics. \n
-#     Otherwise, use web-search. Give a binary choice 'web_search' or 'vectorstore' based on the question. \n
-#     Return JSON with a single key 'datasource' and no premable or explaination. \n
-#     Question to route: {question}""",
-#     input_variables=["question"],
-# )
-
-# question_router = prompt | llm | JsonOutputParser()
-# question = "llm agent memory"
-# docs = retriever.get_relevant_documents(question)
-# doc_txt = docs[1].page_content
-# print(question_router.invoke({"question": question}))
